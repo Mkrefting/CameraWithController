c4a15dde3c9212012cf84eff16538787a2492308
Mkrefting <maxkrefting@yahoo.co.uk>
2021-02-24 19:31:57 +0000

    Initial Commit

---
+++ CameraWithController/CameraWithControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraWithControllerApp.swift
+//  CameraWithController
+//
+//  Created by Max Krefting on 24/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct CameraWithControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
